c1975b6583ec526c8198f2ab294496183521c998
Vojtěch Mareš <vojtech@mares.cz>
2025-09-03 12:27:42 +0200

    feat: add content and basic design
    Signed-off-by: Vojtěch Mareš <vojtech@mares.cz>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,28 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import {defineConfig, fontProviders} from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://www.vojtamares.cz",
+  i18n: {
+    locales: ["cs", "en"],
+    defaultLocale: "cs",
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.google(),
+        name: "National Park",
+        cssVariable: "--font-national-park",
+      },
+    ],
+  },
 });
