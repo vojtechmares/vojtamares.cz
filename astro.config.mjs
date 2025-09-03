c78010a8fe323dc310a4ecb994f5ad6433c0e411
Vojtěch Mareš <vojtech@mares.cz>
2025-09-03 12:29:06 +0200

    chore(deps): add @astro/cloudflare
    Signed-off-by: Vojtěch Mareš <vojtech@mares.cz>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,34 @@
 // @ts-check
-import {defineConfig, fontProviders} from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.vojtamares.cz",
-  i18n: {
-    locales: ["cs", "en"],
-    defaultLocale: "cs",
-    routing: {
-      prefixDefaultLocale: false,
+    site: "https://www.vojtamares.cz",
+
+    i18n: {
+        locales: ["cs", "en"],
+        defaultLocale: "cs",
+        routing: {
+            prefixDefaultLocale: false,
+        },
     },
-  },
-  vite: {
-    plugins: [tailwindcss()],
-  },
-  experimental: {
-    fonts: [
-      {
-        provider: fontProviders.google(),
-        name: "National Park",
-        cssVariable: "--font-national-park",
-      },
-    ],
-  },
+
+    vite: {
+        plugins: [tailwindcss()],
+    },
+
+    experimental: {
+        fonts: [
+            {
+                provider: fontProviders.google(),
+                name: "National Park",
+                cssVariable: "--font-national-park",
+            },
+        ],
+    },
+
+    adapter: cloudflare(),
 });
